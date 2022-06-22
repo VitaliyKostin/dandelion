5b176ea8d2aa3174eb99bf957e541b3fc0025f84
VitaliyKostin <tomasevich@ukr.net>
2022-06-22 14:04:04 +0300

    starter

---
+++ starter-project/app/containers/MyPage/SamplePage.js
@@ -0,0 +1,29 @@
+// file: app/containers/MyPage/SamplePage.js
+
+import React from 'react';
+import { Helmet } from 'react-helmet';
+import { PapperBlock } from 'dan-components';
+
+class SamplePage extends React.Component {
+  render() {
+    const title = 'Dandelion Pro. Blank Page';
+    const description = 'Dandelion Pro';
+    return (
+      <div>
+        <Helmet>
+          <title>{ title }</title>
+          <meta name="description" content={ description } />
+          <meta property="og:title" content={ title } />
+          <meta property="og:description" content={ description } />
+          <meta property="twitter:title" content={ title } />
+          <meta property="twitter:description" content={ description } />
+        </Helmet>
+        <PapperBlock title="Blank Page" desc="Some text description">
+          Content 1112
+        </PapperBlock>
+      </div>
+    );
+  }
+}
+
+export default SamplePage;
